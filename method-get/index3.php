d2e61a4be61cf12f87fcbf5183bddefa4e20807f
Andika Tulus Pangestu <62005221+andikatuluspangestu@users.noreply.github.com>
2020-11-19 12:54:02 +0700

    Update index3.php

--- method-get/index3.php
+++ method-get/index3.php
@@ -8,17 +8,23 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
 
-    <title>Data Buku</title>
+    <title>Hello, world!</title>
   </head>
   <body>
 
   <div class="container">
   <br><br>
-  <div class="card shadow-lg border-0 mb-3" style="max-width: 540px;">
+  <div class="card shadow-lg border-0 mb-3" style="max-width: 100%; height: 520px;">
   <div class="row g-0">
+  <div class="col-md-4">
+      <img src="<?php echo $_GET["gambar"]; ?>" style="width: 70%; margin: 60px;">
+    </div>
     <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $_GET["judul"]; ?></h5>
+      <div class="card-body" style="margin-top: 45px;">
+        <h5 class="card-title"><b><?php echo $_GET["judul"]; ?></b></h5>
+        Karya : <?php echo $_GET['karangan']; ?>
+        <br><br>
+        <?php echo $_GET["deskripsi"]; ?>
         <p class="card-text"><small class="text-muted">No. <?php echo $_GET['ISBN']; ?></small></p>
       </div>
     </div>
